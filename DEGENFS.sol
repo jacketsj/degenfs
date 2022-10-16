26b7bd95f39cdb8424d61fbd747c429bac654a7d
cryptotribexyz <samira.gadri7@gmail.com>
2022-10-16 17:38:16 -0400

    decloud sol

--- DEGENFS.sol
+++ DEGENFS.sol
@@ -7,85 +7,39 @@ pragma solidity >=0.7.0 <0.9.0;
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
-contract DecentralizedDatabase {
-
-    // data owner requests data storer to provide proof that storer still has the data
-    struct ProofRequest {
-        uint256 result;
-        uint256 salt;
-    }
+contract DeCloud {
 
     uint64 public id_count = 0;
 
-    struct StoreDataBid {
+    struct JobBid {
         uint256 amount;
         uint256 expiry;
-        string  data_loc;
+        string  job_loc;
         address owner;
     }
 
-    struct Proof {
-        uint256 pre_result; // result of taking file and salt and hashing together
+    mapping(uint64 => JobBid) public jobBids;
+    mapping(uint64 => address) public jobToCompleter;
+    mapping(uint64 => string) public outputLocs;
+
+    function genUUID() public returns(uint64) {
+        return id_count++;
     }
-
-    mapping(uint64 => StoreDataBid) public storeDataBids;
-    mapping(uint64 => address) public dataToStorer;
-    mapping(uint64 => ProofRequest) public idToProofRequests;
-
-
     /**
      * @dev Store value in variable
      */
-    function storeData_Bid(uint256 amount, uint256 expiry, string memory executable_file) public returns(bool){
-        // creates a StoreDataBid struct with the params passed
-        // maps id to ^this struct variable
-        // store that data inside of storeDataBids
-        storeDataBids[id_count] = StoreDataBid(amount, expiry, executable_file, msg.sender);
-        id_count = id_count + 1;
-        // returns true of all this happens successfully       
-        return true;
+    function createJobBid(uint256 expiry, string memory job_loc) public payable {
+        jobBids[genUUID()] = JobBid(msg.value, expiry, job_loc, msg.sender);
+        
+        // TODO put msg.value into contract wallet
     }
 
-    function acceptDataBid(uint64 storeBidId) public returns(bool) {
-        // maps storeBidId to caller's account address
-        // save in contract state
-        dataToStorer[storeBidId] = msg.sender;
-        return true;
+    function registerJobCompletion(uint64 jobId, string memory output_loc) public {
+        // caller completed the job
+        // should permanently store the location of the output
+        jobToCompleter[jobId] = msg.sender;
+        outputLocs[jobId] = output_loc;
+        // TODO pay the caller the amount for the job
+        // transfer(msg.sender, jobBids[jobId].amount);
     }
-
-    function requestForDataStoredProof(uint64 id, uint256 result, uint64 salt ) public returns(bool){
-        // create a ProofRequest struct variable
-        // map id to ^ variable
-        // store that info to the state
-        idToProofRequests[id] = ProofRequest(result, salt);
-        return true;
-    }
-
-    // pre_result means data and salt hashed
-    function submitDataStoredProof(uint64 id, uint256 pre_result) public returns(bool){
-        // verify that hash(pre_result)=result
-        uint256 temp = uint256(sha256(abi.encodePacked(pre_result)));
-        if(temp == idToProofRequests[id].result) {
-            // pay storer
-
-            // if equal, everything corresponding to the id is cleared, and the storer is paid
-            
-            return true;
-        } 
-
-        return false;
-    }
-
-    function punishLatePayment(uint64 id) public returns(bool) {
-       // if after certain period, proof request is not satisfied
-            // storer will have to pay collatoral damages
-    }
-
-    /**
-    * Helper: shows all the available data bits
-    */
-    function showDataBids() public returns(bool) {
-        return true;
-    }
-
 }
